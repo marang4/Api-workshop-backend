a1aff0f77fbc352163fe407f0c11fcbe16a6492c
Gabriel Marangoni <gabrielmarangoni2@hotmail.com>
2025-09-25 21:12:08 -0300

    entrega final AOO

--- src/main/java/com/senac/Api_workshops/config/JwtFilter.java
+++ src/main/java/com/senac/Api_workshops/config/JwtFilter.java
@@ -28,7 +28,8 @@ public class JwtFilter extends OncePerRequestFilter {
         try{
             if (path.equals("/auth/login") || path.startsWith("swagger-resources")
                     || path.startsWith("/v3/api-docs")
-                    //|| path.startsWith("/")
+                    || path.startsWith("/")
+                    || path.startsWith("/webjars")
                     || path.startsWith("/swagger-ui")) {
                 filterChain.doFilter(request, response);
                 return;
